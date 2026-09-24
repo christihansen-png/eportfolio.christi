06473ef8ae4c8dca817f123f300923b5ef9fc5c8
Christi <christihansen@rocketmail.com>
2026-09-23 21:10:58 -0700

    worked on modal

---
+++ index.js
@@ -0,0 +1,18 @@
+// sCCKszcb11RlWnO4K
+
+function contact(event) {
+  event.preventDefault();
+  // emailjs
+  // .sendForm(
+  //   "service_beautiful",
+  //   "template_h4ftgzz"
+  //   event.target,
+  //   "sCCKszcb11RlWnO4K"
+  // )
+  // .then(() => {
+  //   console.log("This worked 1")
+  // })
+  setTimeout(() => {
+    console.log('it worked 1')
+  }, 500);
+}
