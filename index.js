ce2ab10db1a4613974b0a959d16502f044cd235d
Christi <christihansen@rocketmail.com>
2026-09-24 15:29:33 -0700

    finished media query

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 // sCCKszcb11RlWnO4K
+// service_beautiful
+// template_h4ftgzz
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+  document.body.classList += " dark-theme"
+}
+else {
+  document.body.classList.remove("dark-theme")
+}
+}
 
 function contact(event) {
   event.preventDefault();
-  // emailjs
-  // .sendForm(
-  //   "service_beautiful",
-  //   "template_h4ftgzz"
-  //   event.target,
-  //   "sCCKszcb11RlWnO4K"
-  // )
-  // .then(() => {
-  //   console.log("This worked 1")
-  // })
-  setTimeout(() => {
-    console.log('it worked 1')
-  }, 500);
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible";
+  emailjs
+  .sendForm(
+    "service_beautiful",
+    "template_h4ftgzz",
+    event.target,
+    'sCCKszcb11RlWnO4K'
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    alert(
+      "The email service is temporarily unavailable. Please contact me directly on your_beautiful@yahoo.com"
+    );
+  })
+}
+
+function toggleModal(){
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
